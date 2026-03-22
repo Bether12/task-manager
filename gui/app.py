dbe567a7b39abe8824666e1ea9310ccdd83584d7
Bether12 <elnenogomez13@gmail.com>
2026-03-22 12:39:24 -0400

    Change listbox widget to treeview widget and replace all listbox related methods with treeview's

--- gui/app.py
+++ gui/app.py
@@ -27,15 +27,19 @@ class TaskManager:
 
         tk.Button(self.root, text="Add Task", command=self.add_task, bg="#4CAF50", fg="white", font=("Arial", 10, "bold")).pack(pady=10)
 
-        self.listbox = tk.Listbox(self.root, width=70, height=14, font=("Arial", 11))
-        self.listbox.pack(pady=10)
+        self.tree = ttk.Treeview(self.root, columns=("name", "status", "priority", "date"), show="headings")
+        self.tree.heading("name", text="Task name")
+        self.tree.heading("priority", text="Priority level")
+        self.tree.heading("status", text="Status")
+        self.tree.heading("date", text="Date created")
+        self.tree.pack(pady=5)
 
         self.btn_frame = tk.Frame(self.root)
         self.btn_frame.pack()
         tk.Button(self.btn_frame, text="Mark as done", command=self.mark_done, bg="#2196F3", fg="white").grid(row=0, column=0, padx=10)
         tk.Button(self.btn_frame, text="Delete Task", command=self.delete_task, bg="#f44336", fg="white").grid(row=0, column=1, padx=10)
 
-        self.refresh_list()
+        self.refresh_tree()
 
     def add_task(self):
         name = self.entry.get()
@@ -46,30 +50,33 @@ class TaskManager:
         priority = int(self.priority_val.get()[0])
 
         self.tasks.add_task(name, priority)
-        self.refresh_list()
+        self.refresh_tree()
         self.entry.delete(0, tk.END)
         self.priority_val.set("1(High)")
 
     def mark_done(self):
         try:
-            index = self.listbox.curselection()[0]
+            #Selects the selected item id, splits it and access the relevant number part, then subtracts 1 to be compatible with zeroth indexes
+            index = int(self.tree.selection()[0].split('I')[1]) - 1
             self.tasks.mark_done(index)
-            self.refresh_list()
+            self.refresh_tree()
         except IndexError:
             messagebox.showwarning("Warning", "Select a task")
 
     def delete_task(self):
         try:
-            index = self.listbox.curselection()[0]
+            #Selects the selected item id, splits it and access the relevant number part, then subtracts 1 to be compatible with zeroth indexes
+            index = int(self.tree.selection()[0].split('I')[1]) - 1 
             self.tasks.delete_task(index)
-            self.refresh_list()
+            self.refresh_tree()
         except IndexError:
             messagebox.showwarning("Warning", "Select a task")
     
-    def refresh_list(self):
-        self.listbox.delete(0, tk.END)
+    def refresh_tree(self):
+        for id in self.tree.get_children():
+            self.tree.delete(id)
         for task in self.tasks.task_list:
-            self.listbox.insert(tk.END, f"{task.name}   {task.type}   {str(task.priority)}   {task.date_created}")
+            self.tree.insert("", "end", values=(task.name, task.priority, task.type, task.date_created))
 
     def on_closing(self):
         self.tasks.save_data()
